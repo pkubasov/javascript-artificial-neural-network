920ac5d3be3a2cec21c619d7c0c7e40bddf76298
pkubasov <pkubasov@gmail.com>
2013-08-12 16:27:11 -0400

    Refactored ref implemenation and cleaned up PSO algorithm

--- PSO.js
+++ PSO.js
@@ -1,65 +1,52 @@
 /**
- * Created with IntelliJ IDEA.
- * User: PhilipK
+ * Particle swarm optimization
+ *
+ * Author: Philip Kubasov
  * Date: 7/31/13
  * Time: 9:44 AM
- * To change this template use File | Settings | File Templates.
+ *
  */
 
 // namespace
 var pso = {};
 
-// globally scoped variables
+/**
+ *  # of particles created (during test run or prod run)
+ * @type {Number}
+ */
+pso.globalParticleCounter=0;
 
 /**
  * Determines the degree of social impact on particle's performance
  * @type {Number}
  */
 pso.globalAcceleration = 0.595;
+
 /**
  * Determines the degree of impact of particle's cognition of its own best results on its performance
  * @type {Number}
  */
 pso.defaultLocalAcceleration = 0.985;
+
 /**
- *  delta t, how long a particle is considered to run with current velocity
+ * delta t, how long a particle is considered to run with current velocity
  * @type {Number}
  */
 pso.timeIncrement = 1;
-/**
- *  # of particles created (during test run or prod run)
- * @type {Number}
- */
-pso.globalParticleCounter=0;
+
 /**
  * total number of runs counter
  * @type {Number}
  */
 pso.totalRuns = 0;
-/**
- * array of test particles - used to access them for movement
- * @type {Array}
- */
-pso.particles = [];
-/**
- * determines the effect of current velocity on the next velocity value
- * @type {Number}
- */
-pso.defaultInertia = 0.55;
-/**
- * Max # of runs before we quit
- * @type {Number}
- */
-pso.maxRuns = 10000;
-/*
-    goal parameters for testing
- */
-pso.targetX = 800*Math.random();
-pso.targetY = 700*Math.random();
-pso.targetZ = -10;
 
-// scoring vector
+/**
+ * position vector constructor
+ * @constructor
+ */
 pso.Vector = function() {
+    // sample scoring vector for 3-d motion, could be n-dimensional
+    /*
     this.x = {
         result: 0, value : 0, runsMissingGoal:0
     };
@@ -69,9 +56,11 @@ pso.Vector = function() {
     this.z = {
         result: 0, value : 0, runsMissingGoal:0
     };
-};
+    */
+    throw new Error("Required implementation of Vector constructor function is missing");
 
-pso.gbest = new pso.Vector();
+};
+// pso.gbest = new Vector() ; // should follow implementation of Vector
 
 LOG_LEVEL = 2;
 /**
@@ -95,15 +84,8 @@ pso.log=function (msg, level) {
  */
 pso.ScoreCard = function(scoringFunction, pbest, particleName) {
     this.particleName = particleName;
-    //var timeSlot = new Array();
-    this.pbest = pbest;
 
-    /*
-       this is used for testing/demo purposes and could be extracted
-     */
-    var bestXElement = document.getElementById("bestX");
-    var bestYElement = document.getElementById("bestY");
-    var bestZElement = document.getElementById("bestZ");
+    this.pbest = pbest;
 
     this.addScore = function (val) {
         //timeSlot.push(val);
@@ -123,17 +105,12 @@ pso.ScoreCard = function(scoringFunction, pbest, particleName) {
                     pso.gbest[i] = result[i];
                     this.pbest[i].runsMissingGoal = 0;
                     pso.log(this.particleName + ": New global best: for " + i + ". Score: " + pso.gbest[i].result + ", Value:" + pso.gbest[i].value,1);
-
-                    /*
-                     this is used for testing/demo purposes and could be extracted
-                     */
-                    if(i=="x") bestXElement.innerHTML = pso.gbest[i].value;
-                    if(i=="y") bestYElement.innerHTML = pso.gbest[i].value;
-                    if(i=="z") bestZElement.innerHTML = pso.gbest[i].value;
+                    pso.broadcastResults(i, pso.gbest[i].value);
                 }
             }
         }
     };
+
     this.scoringFunction = scoringFunction;
 };
 
@@ -165,7 +142,7 @@ pso.Particle = function( scoringVector, scoringFn, inertia, localAcceleration, n
     };
     for(var i  in scoringVector) {
         if(scoringVector.hasOwnProperty(i)) {
-            this.vector.pos[i] =  Math.random()*500;
+            this.vector.pos[i] =  Math.random()*screen.availWidth; //close enough to distribute evenly
             this.vector.velocity[i] = Math.random();
         }
     }
@@ -212,7 +189,7 @@ pso.Particle = function( scoringVector, scoringFn, inertia, localAcceleration, n
         this.scoreCard.addScore(this.vector.pos);
 
         //update node's position
-        pso.setPosition(this.node, this.vector.pos.x, this.vector.pos.y, this.vector.z);
+        pso.setPosition(this.node, this.vector.pos);
 
         // slowly start favoring social interaction over cognitive
         this.localAcceleration-=0.0003;
@@ -222,7 +199,7 @@ pso.Particle = function( scoringVector, scoringFn, inertia, localAcceleration, n
     this.start = function(maxRuns) {
         // staggered timeout values
         var t1=611;
-        var t2=923;
+        var t2=623;
         var that = this;
 
         var runInt = setInterval(function(){
@@ -242,39 +219,16 @@ pso.Particle = function( scoringVector, scoringFn, inertia, localAcceleration, n
     };
 };
 
-/**********************************   Test Setup      ****************************************************************
-    vector will be 3-d space represented by x,y and z
-    scoring function will evaluate based on distance from target location = predefined spot of {x: 300, y:100, z: -10 }
-**********************************************************************************************************************/
-
-/**
- * Sample implementation of a success evaluation function
- *
- * @param vector   position vector, in this case for 3 dimensions
- * @return {Object} scoring vector (position vector + scores for each dimension)
- */
-pso.successFn  = function(vector) {
-    var dx = Math.sqrt(Math.pow(vector.x - pso.targetX, 2));
-    var dy = Math.sqrt(Math.pow(vector.y - pso.targetY, 2));
-    var dz = Math.sqrt(Math.pow(vector.z - pso.targetZ, 2));
-
-    return {
-        x: {result: dx == 0? Infinity : 1/dx, value: vector.x},
-        y: {result: dy == 0? Infinity : 1/dy, value: vector.y},
-        z: {result: dz == 0? Infinity : 1/dz, value: vector.z}
-    };
-
-};
-
 /**
  * Determine if we are optimized enough to quit trying any further
+ * This should be overridden as needed
  *
  * @param best current best value vector
  * @return {Boolean}
  */
 pso.isGoodEnough = function(best) {
-    var delta = Math.abs(best.x.value-pso.targetX) + Math.abs(best.y.value- pso.targetY) + Math.abs(best.z.value - pso.targetZ);
-    return delta < 0.5;
+    throw new Error("Required implementation of isGoodEnough function is missing");
+    return false;
 };
 
 /**
@@ -284,109 +238,31 @@ pso.isGoodEnough = function(best) {
  * @return {Element}
  */
 pso.createParticleNode = function (id) {
-    var node = document.createElement("img");
-    node.setAttribute("id", id);
-    node.setAttribute("src","images/ant.gif");
-    node.setAttribute("width", "52px");
-    node.setAttribute("height", "42px");
-    pso.setPosition(node, Math.random()* 1024, Math.random()*768, 1);
-    document.body.appendChild(node);
-    return node;
+   throw new Error("Required implementation of createParticleNode function is missing");
+   return null;
 };
 
 /**
- *  moves the particle around in 2-dimensional space on a page
+ * moves the particle around in n-dimensional space
  *
  * @param node - particle node to move
- * @param x
- * @param y
- */
-pso.setPosition = function(node, x,y, z) {
-    var zIndex = Math.floor(z);
-    node.setAttribute("style", "z-index:" + zIndex + ";position: absolute; top:" + y + "px;left: " + x + "px;");
-    if(node.getAttribute("id") === "goal") {
-        var targetXElement = document.getElementById("targetX");
-        var targetYElement = document.getElementById("targetY");
-        var targetZElement = document.getElementById("targetZ");
-        targetXElement.innerHTML = x+100; // half of target image width is added to center
-        targetYElement.innerHTML = y+100; // half of target image height is added to center
-        targetZElement.innerHTML = pso.targetZ;
-    }
-};
-
-/**
- * Function to move goal node, thereby changing the goal parameters
+ * @param pos -  position vector
  *
- * @param e DOM Event Object
  */
-pso.moveGoalNode = function(e){
-    var id = e.keyIdentifier || e.keyCode;
-    switch(id) {
-        case 'Down':
-        case 40:
-            pso.targetY+=5;
-            pso.globalAcceleration+=0.001;
-            break;
-        case 'Up':
-        case 38:
-            pso.targetY-=5;
-            pso.globalAcceleration+=0.001;
-            break;
-        case 'Left':
-        case 37:
-            pso.targetX-=5;
-            pso.globalAcceleration+=0.001;
-            break;
-        case 'Right':
-        case 39:
-            pso.targetX+=5;
-            pso.globalAcceleration+=0.001;
-            break;
-        default:
-            break;
-    }
-    pso.setPosition(pso.goalNode, pso.targetX-100, pso.targetY-100, 99);
-
+pso.setPosition = function(node, pos) {
+    throw new Error("Required implementation of setPosition function is missing");
 };
 
 /**
- *  function that triggers test runs; gets called on document load
+ *  Optional function to be overridden for publishing results
+ * @param dim
+ * @param value
  */
-pso.startSimulation = function() {
+pso.broadcastResults = function(dim, value) {
 
-    // set up target(goal) DOM Node
-    (function(){
-        var goalNode = document.createElement("img");
-        goalNode.setAttribute("src", "images/honey.png");
-        goalNode.setAttribute("width", "200px");
-        goalNode.setAttribute("height", "200px");
-        goalNode.setAttribute("id", "goal");
-        pso.setPosition(goalNode, pso.targetX-100, pso.targetY-100, 99);
-        document.body.appendChild(goalNode);
-        pso.goalNode = goalNode;
-    })();
+}
 
-    // number of particles to generate
-    var numParticles = 80;
 
-    for(var i=1; i<=numParticles ; i++) {
-        var p = new pso.Particle(new pso.Vector(), pso.successFn, pso.defaultInertia, pso.defaultLocalAcceleration, pso.createParticleNode("p" + i));
-        pso.particles.push(p);
-        p.start(pso.maxRuns);
-    }
-
-    // timeout value
-    var t1=1023;
-
-    // check if simulation is finished
-    var checkInt = setInterval( function() {
-        if(pso.isGoodEnough(pso.gbest)) {
-            clearInterval(checkInt);
-            pso.log("Goal found after " + pso.totalRuns + " runs. Gbest values are: x: " + pso.gbest.x.value + ", y: " + pso.gbest.y.value ,4);
-            pso.totalRuns = 0;
-        }
-    }, t1);
-};
 
 
 
