2e605c9115baa4d2306acb8c2bbcdd70d07b513f
pkubasov <pkubasov@gmail.com>
2017-03-01 15:01:27 -0500

    added functional code from another file

--- Functional.js
+++ Functional.js
@@ -144,3 +144,107 @@ function multRat(x, y) {
     return makeRat(numer(x) * numer(y) , denom(x) * denom(y));
 }
 
+//////////////////   SICP ///////////////////////////
+
+function fixed_point(f,start,tolerance) { 
+	var closeEnough = function(u,v) { return Math.abs(u-v)<tolerance; };
+	if( closeEnough(start, f(start))) { return start;} 
+	else { console.log("Last value: " + start); return fixed_point(f, f(start), tolerance);} 
+}
+
+function averageDamp(f) {
+	// private function 
+    var average=function(x, y) { return (x+y)/2; }
+	
+    function sqrt(x) { 
+	   // private function to average damp - the function whose fixed point approaches the value of square root
+	   var f=function(y) {return x/y}; 
+	   return fixed_point(averageDamp(f), 1, 0.00001); 
+    }
+    
+	return function(x) {
+		return average(x, f(x));
+	};
+}
+
+
+
+function derivative (f) {
+	return function(x) {
+		var dx = 0.0001;
+		return (f(x)-f(x+dx))/dx;
+	};
+}
+
+function newtonMethod(f, x) {
+	var df = derivative(f);	
+	return fixed_point(function(y) { return y - f(y)/df(y);} , x, 0.0001);
+}
+
+/**
+* determines if n is a power of 2
+*/
+function isP2(n) { 
+	return (n & (n - 1)) == 0;
+}
+
+function gcd2(x,y) {
+	var lesser = x>y ? y : x;
+	var greater = x>y? x : y;
+	
+	if(greater%lesser ==0) return lesser;
+	
+	var x=1;
+	var gcdNum = 1;	
+	
+	while(Math.floor(lesser/x++)>1) {
+		var mod1, mod2;
+		if(isP2(x)) {
+			// x is a power of two
+			mod1 = greater & (x-1);
+			mod2 = lesser & (x-1);
+		} else {
+			mod1 = greater%x;
+			mod2 = lesser%x
+		}
+		
+		if(mod1 ==0 &&  mod2 == 0) {
+			gcdNum*= x;
+			greater/=x;
+			lesser/=x;
+			x=1;
+		}
+	}
+	return gcdNum;
+}
+
+function cons(a,b) {
+	return function(x) {
+	    if(x!=1 && x!=2) throw new Error("Arguments can only be 1 or 2");
+		return x==1? a: b;
+	};
+}
+
+function car(f) {
+	if(f==null) { return null; /* reached end of the list */ }
+	if(typeof f!=='function') throw new Error("Invalid object used as argument");
+	return f(1);
+}
+
+function cdr(f) {
+	if(f==null) { return null; /* reached end of the list */ }
+	if(typeof f!=='function') throw new Error("Invalid object used as argument");
+	return f(2);
+}
+
+function list() {
+	if(arguments.length==0) {
+		return null;
+	} else {
+		return cons( arguments[0], arguments.callee.apply(this, Array.prototype.slice.call(arguments,1)));
+	}
+}
+
+
+
+
